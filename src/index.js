2c6a66977cb457cdb8742c84c729499d14485a2c
d3mchenko <maksim-ignatyuk@mail.ru>
2021-03-13 14:32:25 +0300

    fix:update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(typeof array != "undefined" && array != null && array.length>0) {
+  let min_array = array[0];
+  for (let i=1; i<array.length; i++) {
+    if (array[i]<min_array) {
+      min_array=array[i];
+    }
+  }
+  return min_array;
+}
+return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(typeof array != "undefined" && array != null && array.length>0) {
+  let max_array = array[0];
+  for (let i=1; i<array.length; i++) {
+    if (array[i]>max_array) {
+      max_array=array[i];
+    }
+  }
+  return max_array;
+}
+return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(typeof array != "undefined" && array != null && array.length>0) {
+  let avg_array = 0;
+  let sum=0;
+  for (let i=0; i<array.length; i++) {
+    sum+=array[i];
+  }
+  avg_array = sum / array.length;
+  return avg_array;
+}
+return 0;
 }
